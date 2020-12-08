571556cbe9e3fb581342c319c82a0945f4194cb4
Diwas Paudel <diwasp1@gmail.com>
2020-12-09 00:39:21 +0545

    all

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,28 +1,31 @@
-import React from 'react'
-import Header from '../Components/Header'
-import Card from '../Components/Card'
+import React from "react";
+import Header from "../Components/Header";
+import Card from "../Components/Card";
 import Button from "../Components/Button";
-import database from '../firebase';
+import database from "../firebase";
 
 function Home() {
-    const handleLogout = () => {
-        database.auth().signOut().then(function() {
-            // Sign-out successful.
-          }).catch(function(error) {
-            // An error happened.
-          });
-    }
-    return (
-        <>
-            <Card />
-            <Button />
-            <div>
-                <button onClick={handleLogout}>
-                    Logout
-                </button>
-            </div>
-        </>
-    )
+  const handleLogout = () => {
+    database
+      .auth()
+      .signOut()
+      .then(function () {
+        // Sign-out successful.
+      })
+      .catch(function (error) {
+        // An error happened.
+      });
+  };
+  return (
+    <>
+      <Header />
+      <Card />
+      <Button />
+      <div>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
